1f9d35158d4829866b3583f7ff9a65ad3fb005e4
Sumana <sumanasridharan@gmail.com>
2024-05-12 22:00:43 +0530

    ==

--- src/pages/Books.py
+++ src/pages/Books.py
@@ -45,7 +45,7 @@ cents = books.DegreeCentrality(graphs)
 
 books.gen_centrality_plot(cents)
 try:
-    pth = "/pages/tmp/"
+    pth = "/tmp/"
     img = Image.open(f"{pth}centrality.png")
 except:
     pth = "pages/"
